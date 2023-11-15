191583e67754a8a03b6f907409ba681995f9ca5d
M2K Developments <m2ksoftiez@gmail.com>
2023-11-15 12:45:48 +0200

    update

--- website/public/service-worker.js
+++ website/public/service-worker.js
@@ -26,10 +26,10 @@ const messaging = firebase.messaging();
 // - a message is received while the app has focus
 // - the user clicks on an app notification created by a service worker
 //   `messaging.onBackgroundMessage` handler.
-messaging.onMessage((payload) => {
-    console.log('Message received. ', payload);
-    // ...
-});
+// messaging.onMessage((payload) => {
+//     console.log('Message received. ', payload);
+//     // ...
+// });
 
 
 messaging.onBackgroundMessage((payload) => {
